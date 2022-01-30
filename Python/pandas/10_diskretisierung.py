da1cb1779083efaf99693710739c1def07ede3a9
Thomas <thomas-strehler@gm.xde>
2022-01-30 01:27:46 +0100

    pandas tutorial diskretisierung

--- Python/pandas/10_diskretisierung.py
+++ Python/pandas/10_diskretisierung.py
@@ -19,5 +19,30 @@ print(pd.value_counts(cats))
 # quantile list muss von range[0, 1] sein
 perc = pd.qcut(df, [0, 0.1, 0.2, 0.5, 0.75, 1])
 print("===================")
-print(perc)
-print(pd.value_counts(perc))
+# print(perc)
+# print(pd.value_counts(perc))
+
+# Erstellen von Kategorien -> bei uns ACD
+#ordered=True -> übernimmt dann unsere Ordnung
+cat = pd.Categorical(
+    list("ABCA"), categories=list("ACD"), ordered=False
+)
+print(cat)
+# B war schon vorher weg
+cat = cat.add_categories(['B'])
+
+print(cat.remove_categories(["A"]))
+
+#entfernt alle nicht verwendete Kategorien
+cat = cat.remove_unused_categories()
+print(cat)
+
+# ordered=True -> übernimmt dann unsere Ordnung
+# !! bla.sort_values, komischerweise ist ordered=True/False egal
+cat2 = pd.Categorical(
+    list("ABCADDEEA"), categories=list("CAD"), ordered=False
+)
+print("========ordered=========")
+cat2.sort_values(inplace=True)
+print(cat2)
+
