a5ca1d6c2a0bc3d3be3352dd2e8592124464c19e
Thomas <thomas-strehler@gm.xde>
2022-04-03 10:54:03 +0200

    exercise 2.2

--- Cplusplus/Bootcamp/02_ArrayPointer/2_2_Exercise/exercise.cc
+++ Cplusplus/Bootcamp/02_ArrayPointer/2_2_Exercise/exercise.cc
@@ -5,9 +5,27 @@
 // Exercise 1
 void push_back(int *&input_array, const unsigned int &size, const int &value)
 {
+    int *ext_array = new int[size + 1];
+    for (unsigned int i = 0; i < size; i++)
+    {
+        ext_array[i] = input_array[i];
+    }
+    delete[] input_array;
+    ext_array[size] = value;
+
+    input_array = ext_array;
 }
 
 // Exercise 2
 void pop_back(int *&input_array, const unsigned int &size)
 {
+    int *ext_array = new int[size - 1];
+    for (unsigned int i = 0; i < size - 1; i++)
+    {
+        ext_array[i] = input_array[i];
+    }
+
+    delete[] input_array;
+
+    input_array = ext_array;
 }
